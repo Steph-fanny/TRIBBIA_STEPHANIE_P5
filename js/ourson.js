f35b26c754d0e1d7a8e7b99c324f485cdb10b046
steph-fanny <stephdvweb@gmail.com>
2021-05-17 10:52:46 +0200

    couleurs ourson p2

--- js/ourson.js
+++ js/ourson.js
@@ -29,33 +29,34 @@ let address = url.searchParams.get("address");
                 const img = document.getElementById("product_img");
                     img.src = product.imageUrl;                    
                 document.getElementById("product_description").textContent = product.description                
-                document.getElementById("product_price").textContent = product.price +" euros"
+                document.getElementById("product_price").textContent ="Prix : "+ product.price +" euros"
                 document.getElementById("product_title").textContent = product.name
-                // document.getElementById("product_color").textContent = "choisissez votre couleur: "+ product.colors
-                // document.getElementById("inlineFormCustomSelectPref").textContent = product.colors
-               
                 
+                // Choix des couleurs
+                const firstColor = document.getElementById("first_color");
+                firstColor.innerText =  product.colors[0]
+
+                const secondColor = document.getElementById("second_color");
+                secondColor.innerText =  product.colors[1]
+               
+                const thirdColor = document.getElementById("third_color");
+                thirdColor.innerText =  product.colors[2]
+
+                const fourthColor = document.getElementById("fourth_color");
+                fourthColor.innerText =  product.colors[3]
+
+                // 2 eme essais couleurs
+                // document.getElementById("product_color").innerText = product.colors
+                // console.log(product_color)
+                                              
             })   
-                      
+               
+            
+
             .catch(function(error){ 
                 alert(error)     
                 console.log('erreur: '+ err);        
              }) 
                      
         }     
-              
-        
-        
-   
-
-        
-                 
- 
-
-     
-
-
-
-
-
 
